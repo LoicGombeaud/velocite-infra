298cca6e1d1c958d7ae2c8bf87eecd38fffdfb85
Loïc Gombeaud <loic.gombeaud@gmail.com>
2024-01-20 14:25:24 +0100

    fix(backend): Allow compatibility with Terraform >= 1.6
    As per https://help.ovhcloud.com/csm/en-public-cloud-compute-terraform-high-perf-object-storage-backend-state\?id\=kb_article_view\&sysparm_article\=KB0051345\#initializing-terraform-configuration

--- backend.tf
+++ backend.tf
@@ -1,10 +1,14 @@
 terraform {
   backend "s3" {
-    bucket                      = "velo-cite"
-    key                         = "terraform.tfstate"
-    region                      = "gra"
-    endpoint                    = "s3.gra.io.cloud.ovh.net"
+    bucket = "velo-cite-data"
+    key    = "terraform.tfstate"
+    region = "gra"
+    endpoints = {
+      s3 = "https://s3.gra.io.cloud.ovh.net/"
+    }
     skip_credentials_validation = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
